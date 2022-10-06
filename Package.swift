68ba9c2670e66cf6f41e32690ed89bcb53417a41
Ermat <ealymbaev@gmail.com>
2022-10-06 17:48:36 +0600

    Organize and update source code

--- Package.swift
+++ Package.swift
@@ -3,22 +3,26 @@
 import PackageDescription
 
 let package = Package(
-    name: "CurrencyKit",
-    platforms: [
-        .iOS(.v13),
-    ],
-    products: [
-        .library(
-            name: "CurrencyKit",
-            targets: ["CurrencyKit"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.1")),
-        .package(url: "https://github.com/horizontalsystems/StorageKit.git", .upToNextMajor(from: "1.0.0")),
-    ],
-    targets: [
-        .target(
-            name: "CurrencyKit",
-            dependencies: ["RxSwift", "StorageKit"])
-    ]
+        name: "CurrencyKit",
+        platforms: [
+            .iOS(.v13),
+        ],
+        products: [
+            .library(
+                    name: "CurrencyKit",
+                    targets: ["CurrencyKit"]),
+        ],
+        dependencies: [
+            .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.1")),
+            .package(url: "https://github.com/horizontalsystems/StorageKit.Swift.git", .upToNextMajor(from: "1.0.0")),
+        ],
+        targets: [
+            .target(
+                    name: "CurrencyKit",
+                    dependencies: [
+                        "RxSwift",
+                        .product(name: "StorageKit", package: "StorageKit.Swift"),
+                    ]
+            )
+        ]
 )
